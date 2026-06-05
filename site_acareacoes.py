c57aec8e19b9b126ba86715e215558e08278bda5
arrudapointexe <itallo.mccg@gmail.com>
2026-06-05 16:17:03 -0300

    Update site_acareacoes.py

--- site_acareacoes.py
+++ site_acareacoes.py
@@ -1,7 +1,7 @@
 import os
 import streamlit as st
 import pandas as pd
-import urllib.parse
+import urllib.parser
 import re
 import gspread
 import json
@@ -179,7 +179,7 @@ if menu == "📷 Portal do Motorista":
                             prazo_texto = formatar_prazo(row.get('Prazo do Processo', ''))
 
                             st.info(f"⏰ PRAZO DE FECHAMENTO: {prazo_texto}")
-                            st.info(f"💰 VALOR DO PACOTE: R//$ {row.get('Valor', '0.00')} + R$ 100,00 MULTA")
+                            st.info(f"💰 VALOR DO PACOTE: RS {row.get('Valor', '0.00')} + R$ 100,00 MULTA")
 
                             st.markdown("### 📷 Enviar Comprovante")
                             foto = st.file_uploader(f"Anexe o print/foto (AWB {row['AWB']})", type=['png', 'jpg', 'jpeg'], key=f"file_{row['AWB']}")
